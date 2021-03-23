464b537074c10c0b6fbf6db67ce0a7888cefc380
Hans-Christian Otto <c.otto@suora.com>
2021-03-23 15:33:04 +0100

    TS fun

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import ReactDOM from "react-dom";
 import App, { foo } from "./App";
+import { Message } from "./domain/Message";
 console.log(foo);
 
 ReactDOM.render(<App />, document.getElementById("root"));
+// const message: Pick<Message, Exclude<keyof Message, "id">> = {
+
+type UnsavedMessage = Omit<Message, "id">;
+
+const message: UnsavedMessage = {
+  message: "dasdsa",
+  author: "as",
+  date: 213,
+};
