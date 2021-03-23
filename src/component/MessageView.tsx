b9e7e499054886608e2205905a986cb588d6ba50
Hans-Christian Otto <c.otto@suora.com>
2021-03-23 14:06:24 +0100

    React.FunctionComponent

--- src/component/MessageView.tsx
+++ src/component/MessageView.tsx
@@ -1,15 +1,20 @@
+import React from "react";
+
 interface Props {
   opinion: string;
   name: string;
 }
 
-export function MessageView({ name, opinion }: Props) {
+export const MessageView: React.FunctionComponent<Props> = ({
+  name,
+  opinion,
+}) => {
   return (
     <article>
       {opinion}
       <footer>{name}</footer>
     </article>
   );
-}
+};
 
 export default MessageView;
