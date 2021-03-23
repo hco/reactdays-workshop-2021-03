fad8ba97d073df85bc846b57b919ea83522c2cfa
Hans-Christian Otto <c.otto@suora.com>
2021-03-23 16:26:20 +0100

    Added input field to messagecompose

--- src/component/MessageCompose.tsx
+++ src/component/MessageCompose.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export const MessageCompose: React.FunctionComponent = () => {
+  const [inputValue, setInputValue] = useState("");
+
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
     console.log("Message was sent");
   };
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(event.target.value);
+    setInputValue(event.target.value);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
+      {inputValue} <br />
+      <input onChange={handleChange} />
       <button type="submit">Send Message!</button>
     </form>
   );
