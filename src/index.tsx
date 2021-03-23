8599429b41eab47b6649c875dfdfbd3751e13ccc
Hans-Christian Otto <c.otto@suora.com>
2021-03-23 11:27:27 +0100

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom";
 
-console.log("Hello World!");
+const element = <h1>Hallo Zoom! {Math.PI}</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
