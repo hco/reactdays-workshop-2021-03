70eae82eb740d31ca741200b4b176d9dcf782480
Hans-Christian Otto <c.otto@suora.com>
2021-03-23 13:59:08 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MessageView from "./component/MessageView";
 
 function App() {
+  const city = "Witten";
+
   return (
     <>
       <h2>Hallo Pouria</h2>
-      <MessageView opinion="Ich mag React!" name="Christian" />
+      <MessageView opinion="Ich mag React!" name={`Christian aus ${city}`} />
     </>
   );
 }
