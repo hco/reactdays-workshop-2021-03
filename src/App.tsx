fe11ec99615f6b669124d184b9f152b05d04bc5b
Hans-Christian Otto <c.otto@suora.com>
2021-03-23 14:50:56 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import MessageView from "./component/MessageView";
+import { Message } from "./domain/Message";
 
 function App() {
   const city = "Witten";
+  const message: Message = {
+    author: "Christian",
+    date: 1234,
+    message: "Hallo Welt!",
+    id: "sdajudajs",
+  };
 
   return (
     <>
       <h2>Hallo Pouria</h2>
-      <MessageView
-        message={{
-          author: "Christian",
-          date: 1234,
-          message: "Hallo Welt!",
-          id: "asdjidasidjsais",
-        }}
-      />
+      <MessageView message={message} />
     </>
   );
 }
