b46f561ace4fd90f51607c14531cb6ba5ccb43e7
Hans-Christian Otto <c.otto@suora.com>
2021-03-24 12:15:10 +0100

    WIP

--- src/component/MessageList.tsx
+++ src/component/MessageList.tsx
@@ -3,28 +3,7 @@ import React, { useEffect, useState } from "react";
 import MessageView from "./MessageView";
 
 export const MessageList: React.FunctionComponent = () => {
-  const messages: Message[] = [
-    {
-      id: "213321",
-      author: "Christoph",
-      date: 123,
-      message: "adsdas",
-    },
-    {
-      id: "213325",
-      author: "Patrick",
-      date: 124,
-      message: "adsdas",
-    },
-    {
-      id: "213326",
-      author: "Paul",
-      date: 125,
-      message: "adsdas",
-    },
-  ];
-
-  const [messagesState, setMessagesState] = useState(messages);
+  const [messagesState, setMessagesState] = useState<Message[]>([]);
 
   useEffect(() => {
     setTimeout(() => {
